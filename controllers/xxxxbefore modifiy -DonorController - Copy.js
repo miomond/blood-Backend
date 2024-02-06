2997841632f64d584578d4b3430e1e42fdc341c3
mostafa <[eng.mustapha45002@gmail.com]>
2024-02-06 23:03:35 +0200

    crud-1  6-2-2025

--- controllers/xxxxbefore modifiy -DonorController - Copy.js
+++ controllers/xxxxbefore modifiy -DonorController - Copy.js
@@ -1,5 +1,5 @@
  ////////////// Donor Controller ///////////////
-const Donor = require('../models/donor');
+const Donor = require('../models/Donor');
 
 ///////// Function to get all donors ////////////
 async function getAllDonors() {
@@ -72,79 +72,4 @@ getDonorById,
 createDonor,
 updateDonor,
 deleteDonor
-};
-///////////////// Receiver Controller /////////////////
-const Receiver = require('../models/receiver');
-
-/////// Function to get all receivers /////////////
-async function getAllReceivers() {
-try {
-const receivers = await Receiver.find();
-return receivers;
-} catch (error) {
-console.log(error);
-}
-}
-
-/////// Function to get a single receiver by ID /////////
-async function getReceiverById(id) {
-try {
-const receiver = await Receiver.findById(id);
-if (!receiver) {
-console.log('Receiver not found');
-}
-return receiver;
-} catch (error) {
-console.log(error);
-}
-}
-
-//////// Function to create a new receiver ///////////////
-async function createReceiver(receiverData) {
-try {
-const { name, bloodType, location, contact } = receiverData;
-const newReceiver = new Receiver({ name, bloodType, location, contact });
-const savedReceiver = await newReceiver.save();
-return savedReceiver;
-} catch (error) {
-console.log(error);
-}
-}
-
-//////////// Function to update a receiver ///////////////
-async function updateReceiver(id, receiverData) {
-try {
-const { name, bloodType, location, contact } = receiverData;
-const updatedReceiver = await Receiver.findByIdAndUpdate(
-id,
-{ name, bloodType, location, contact }
-);
-if (!updatedReceiver) {
-console.log('Receiver not found');
-}
-return updatedReceiver;
-} catch (error) {
-console.log(error);
-}
-}
-
-///////// Function to delete a receiver ////////////////
-async function deleteReceiver(id) {
-try {
-const deletedReceiver = await Receiver.findByIdAndDelete(id);
-if (!deletedReceiver) {
-console.log('Receiver not found');
-}
-console.log('Receiver deleted successfully');
-} catch (error) {
-console.log(error);
-}
-}
-
-module.exports = {
-getAllReceivers,
-getReceiverById,
-createReceiver,
-updateReceiver,
-deleteReceiver
 };
